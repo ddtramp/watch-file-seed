456a123c23d36f2cf284917c9da7da850c146358
ddtramp <wangxichao001@gmail.com>
2014-11-21 15:41:48 +0800

    add core
    npm install and bower install

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    var appConfig = {
+        app: require('./package.json').appPath || 'app',
+        dist: 'dist'
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= pkg.appPath %>/{,*/}*.html',
+                    '<%= pkg.appPath %>/styles/{,*/}*.css',
+                    '<%= pkg.appPath %>/scripts/{,*/}*.js',
+                    '<%= pkg.appPath %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: '<%= pkg.ipAddress %>',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect) {
+                        return [
+                            connect().use('/bower_components', connect.static('./bower_components')),
+                            connect.static(appConfig.app) // can not use <%= pkg.appPath %> get bath path
+                        ];
+                    }
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+        if (target === 'dist') {
+            grunt.log.warn('sorry, i can not config run this server, you can add some task for this...');
+            return;
+        }
+        grunt.task.run([
+            "connect:livereload",
+            "watch"
+        ]);
+    });
+    grunt.registerTask('server', 'DEPERCATED TASK, Use the "serve" task instead', function (target) {
+        grunt.log.warn('the `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve:' + target]);
+    });
+
+    grunt.registerTask('test', []);
+
+    grunt.registerTask('build', []);
+
+    grunt.registerTask('default', [
+        'serve'
+    ]);
+
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# READEME
+----
+2014-11-21 15:31
+
+## grunt file watch and static server
+
+[grunt](#http://gruntjs.com/)
+
+[grunt-contrib-connect](#https://www.npmjs.org/package/grunt-contrib-connect)
+
+[grunt-contrib-watch](#https://www.npmjs.org/package/grunt-contrib-watch)
+
+[load-grunt-tasks](#https://www.npmjs.org/package/load-grunt-tasks)
+
+## notice
+
+you should change package.json `appPath` and `ipAddress`; 
+
+and 
+
+i only add one command 
+
+    serve or server
+    
+    the default is serve
